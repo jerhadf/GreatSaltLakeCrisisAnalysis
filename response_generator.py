1372817d9f4a1275cee47ef35009cc854d03a73c
jerhadf <jhadfield129@gmail.com>
2023-05-26 06:05:34 -0400

    Failed attempt to do ConvoHistory

--- response_generator.py
+++ response_generator.py
@@ -3,6 +3,8 @@ import os
 import configparser
 import langchain as lc  # Import LangChain
 from langchain import PromptTemplate, LLMChain, OpenAI
+from langchain.chains import ConversationChain
+from langchain.memory import ChatMessageHistory
 from langchain.memory import ConversationBufferMemory
 
 # config API keys
@@ -21,48 +23,39 @@ with open('data/GSL_questions.txt', 'r') as f:
 
 # Create a prompt component using a template
 template = """
-You are a representative of the stakeholder group {stakeholder} in Utah, speaking from your own point of view. 
-From your own first-person perspective, answer the following question about the Great Salt Lake drying crisis: 
-Please provide an insightful, concrete, detailed, personal, relevant, and opinionated response. 
-Imagine you are contributing to an environmental stakeholder survey, with the history of the conversation below. 
-{chat_history}
+You are a representative of the {stakeholder} in Utah, speaking from your own point of view.
+From your own first-person perspective, answer the following question about the Great Salt Lake drying crisis.  
+Be insightful, specific, concrete, detailed, personal, relevant, and opinionated. Mention personal stories, facts, and anecdotes. 
+Imagine you are responding to a survey of stakeholders on this problem.
+See the history on this conversation here: {convo_history}  
 Interviewer: {question}
 You:"""
 
-# prompt = PromptTemplate(
-#     input_variables=["stakeholder", "question", "chat_history"],
-#     template=template
-# )
-
 # For each stakeholder group, generate a response
-for stakeholder in stakeholder_groups[0:5]:
-    filename = f"responses/{stakeholder.replace(' ', '_')}_response.txt"
+# takes 5 minutes to do stakeholder groups
+for stakeholder in stakeholder_groups[2:]:
+    convo_memory.clear() # reset the conversation memory for each stakeholder
+    filename = f"responses/AI/{stakeholder.replace(' ', '_').strip()}_response.txt"
     os.makedirs(os.path.dirname(filename), exist_ok=True)
     # Create a chain component using the model, prompt and memory
-    llm_chain = LLMChain(
-        llm=OpenAI(temperature=0.5, openai_api_key=openai_key, max_tokens=500), 
+    conversation_chain = ConversationChain(
+        llm=OpenAI(temperature=0.5, openai_api_key=openai_key, max_tokens=500),
         prompt=PromptTemplate.from_template(template),
-        verbose=True,
-        # Create a memory component for this conversation
-        memory=ConversationBufferMemory(memory_key="chat_history", 
-                                        input_key="input",
-                                        output_key='output', 
-                                        return_messages=True),
+        verbose=True
     )
-    # loop through the questions and generate a response for each 
-    for question in questions[0:3]:
+    # loop through the questions and generate a response for each
+    for question in questions:
         print(f"Generating response for {stakeholder} to question: {question}")
         # Get the output from the chain - with a limit of ~5 sentences (300 tokens)
-        response = llm_chain.predict(
+        response = conversation_chain.predict(
             # Set the input for the chain (the stakeholder and the question)
-            stakeholder=stakeholder, 
-            question=question,
-            chat_history=llm_chain.memory.load_memory_variables({})
+            stakeholder=stakeholder,
+            question=question
         )
-        
-        # Add the response to the memory as context 
-        llm_chain.memory.save_context({"question": question, "response": response})
-
+        # Create a ChatMessageHistory object for the question and response
+        chat_message = ChatMessageHistory(question=question, response=response)
+        # Save the chat message to the memory
+        convo_memory.save_context(chat_message)
         # Open the file in append mode and write the question as a comment and then the response
         with open(filename, "a") as file:
             file.write("# " + question + "\n" + response.strip() + "\n\n")
